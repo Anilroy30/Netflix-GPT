692dafc71f2f5ae38694b155faa8aac785d6eade
Striver <anilkumarakula34@gmail.com>
2025-03-02 12:31:10 +0530

    built Header feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Body from "./components/Body";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world</h1>
+      <Body/>
     </div>
   );
 }
