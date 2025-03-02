11db89f828a6213a48af115d5003a2b897c7ba3a
Striver <anilkumarakula34@gmail.com>
2025-03-02 23:12:43 +0530

    built Sign In authentication using Firebase Successfully

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import Header from './Header'
 import { useRef, useState } from 'react'
 import { checkValidData } from '../utils/Validate'
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../utils/firebase"
 
 const Login = () => {
@@ -38,7 +38,21 @@ const Login = () => {
                 // ..
             });
         }
-        
+        else{
+            // Sign In Form
+            signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log(user);
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode + " - " + errorMessage);
+            });
+        }
     }
 
     const toggleSignInForm = () => {
